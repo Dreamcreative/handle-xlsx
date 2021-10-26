8f7db2cb6f96fe6539319f0386c3f85f22fa688f
maxiao <m543438924@163.com>
2021-10-26 13:43:43 +0800

    feat: 修改汇总表的生成逻辑

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const xlsx = require("node-xlsx");
 const fs = require("fs");
 const {
+	totalkey1,
 	totalkey2,
 	totalkey3,
 	totalkey4,
@@ -42,35 +43,50 @@ totalHeader.forEach((name, index) => {
 	indexMap[name] = index;
 });
 result[0] = totalHeader;
+// 地市 索引
+const cityIndex = indexMap[meridiankey7];
+// 集团名称 索引
+const nameIndex = indexMap[meridiankey8];
+// e55计费号 索引
+const e55Index = indexMap[totalkey1];
+// 投诉编码 索引
+const complaintNumberIndex = indexMap[totalkey2];
+// 出账收入 索引
+const outaccountIndex = indexMap[totalkey3];
+// 码号数量 索引
+const codeNumberIndex = indexMap[totalkey4];
+// 账户并发 索引
+const accountIndex = indexMap[totalkey5];
+// 平均峰值并发 索引
+const averagePeakIndex = indexMap[totalkey6];
+// 投诉总量 索引
+const complaintsIndex = indexMap[totalkey7];
+// 催收投诉总量 索引
+const collectionIndex = indexMap[totalkey8];
+// 是否AI 索引
+const aiIndex = indexMap[totalkey9];
 for (let key in meridianData) {
-	const item = [];
-	// 地市 索引
-	const cityIndex = indexMap[meridiankey7];
-	// 集团名称 索引
-	const nameIndex = indexMap[meridiankey8];
-	// e55计费号 索引
-	const e55 = key;
-	// 投诉编码 索引
-	const complaintNumberIndex = indexMap[totalkey2];
-	// 出账收入 索引
-	const outaccountIndex = indexMap[totalkey3];
-	// 码号数量 索引
-	const codeNumberIndex = indexMap[totalkey4];
-	// 账户并发 索引
-	const accountIndex = indexMap[totalkey5];
-	// 平均峰值并发 索引
-	const averagePeakIndex = indexMap[totalkey6];
-	// 投诉总量 索引
-	const complaintsIndex = indexMap[totalkey7];
-	// 催收投诉总量 索引
-	const collectionIndex = indexMap[totalkey8];
-	// 是否AI 索引
-	const aiIndex = indexMap[totalkey9];
-	// TODO 映射表中需要 添加 投诉编号 字段
-	const complaintNumber = '';
+	const item = new Array(totalHeader.length).fill(complaintNumberDefault);
 	// const [e55, complaintNumber] = key.split(totalSpilt);
 	// const item = data[key];
+	const { data = 0, isAI = false, city = '', name = '' } = meridianData[key];
+	item[cityIndex] = city;
+	item[nameIndex] = name;
+	// 出账收入
+	item[outaccountIndex] = data ?? '';
+	// 是否AI
+	item[aiIndex] = isAI ? "是" : "否";
+	item[e55Index] = key;
 
+	// 码号数量 账户并发 平均峰值并发
+	const { name: companyName = '', complaintNumber = '' } = mappingData[key] || {};
+	const valueObj = trinaData[companyName];
+	item[complaintNumberIndex] = complaintNumber;
+	if (valueObj) {
+		item[codeNumberIndex] = valueObj[trinakey2];
+		item[accountIndex] = valueObj[trinakey3];
+		item[averagePeakIndex] = valueObj[trinakey4];
+	}
 	// 投诉
 	if (complaintNumber !== complaintNumberDefault) {
 		const tempcomplaint = complainData[complaintNumber];
@@ -78,19 +94,6 @@ for (let key in meridianData) {
 			item[complaintsIndex] = tempcomplaint[complaintkey2]
 			item[collectionIndex] = tempcomplaint[complaintkey3]
 		}
-	}
-
-	// 出账收入
-	item[outaccountIndex] = meridianData[e55]?.data ?? '';
-	// 是否AI
-	item[aiIndex] = meridianData[e55]?.isAI ? "是" : "否";
-	// 码号数量 账户并发 平均峰值并发
-	const companyName = mappingData[e55];
-	const valueObj = trinaData[companyName];
-	if (valueObj) {
-		item[codeNumberIndex] = valueObj[trinakey2];
-		item[accountIndex] = valueObj[trinakey3];
-		item[averagePeakIndex] = valueObj[trinakey4];
 	}
 	result.push(item);
 }
