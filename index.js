776efe584673d05f7b33b42164f38ac8ad03ee66
maxiao <m543438924@163.com>
2021-12-30 10:34:44 +0800

    feat:设置默认值

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const xlsx = require("node-xlsx");
-const fs = require("fs");
+const xlsx = require('node-xlsx');
+const fs = require('fs');
 const {
-	totalkey1,
-	totalkey2,
-	totalkey3,
-	totalkey4,
-	totalkey5,
-	totalkey6,
-	totalkey7,
-	totalkey8,
-	totalkey9,
-	totalSpilt,
-	complaintNumberDefault,
+  totalkey1,
+  totalkey2,
+  totalkey3,
+  totalkey4,
+  totalkey5,
+  totalkey6,
+  totalkey7,
+  totalkey8,
+  totalkey9,
+  totalSpilt,
+  complaintNumberDefault,
 
-	complaintkey2,
-	complaintkey3,
-	trinakey2,
-	trinakey3,
-	trinakey4,
-	outputDir,
-	totalHeader,
-	meridiankey7,
-	meridiankey8,
-	deleteFolderRecursive
+  complaintkey2,
+  complaintkey3,
+  trinakey2,
+  trinakey3,
+  trinakey4,
+  outputDir,
+  totalHeader,
+  meridiankey7,
+  meridiankey8,
+  deleteFolderRecursive
 } = require('./utils');
 deleteFolderRecursive(`./${outputDir}`);
-fs.mkdir(`./${outputDir}`, { recursive: true }, (err) => {
-	if (err) throw err;
-});;
+fs.mkdir(`./${outputDir}`, { recursive: true }, err => {
+  if (err) throw err;
+});
 const result = [];
 const complain = require('./src/complain');
-const complainData = complain.data;
+const complainData = complain.data || {};
 const meridian = require('./src/meridian');
-const meridianData = meridian.data;
+const meridianData = meridian.data || {};
 const trina = require('./src/trina');
-const trinaData = trina.data;
+const trinaData = trina.data || {};
 const mapping = require('./src/mapping');
-const mappingData = mapping.data;
+const mappingData = mapping.data || {};
 // const { data, indexMap } = require('./src/total');
 const indexMap = [];
 totalHeader.forEach((name, index) => {
-	indexMap[name] = index;
+  indexMap[name] = index;
 });
 result[0] = totalHeader;
 // 地市 索引
@@ -66,40 +66,40 @@ const collectionIndex = indexMap[totalkey8];
 // 是否AI 索引
 const aiIndex = indexMap[totalkey9];
 for (let key in meridianData) {
-	// const [e55, complaintNumber] = key.split(totalSpilt);
-	// const item = data[key];
-	const { data = 0, isAI = false, city = '', name = '', isNationalVoice = false } = meridianData[key];
-	// 如果不是全国语音，不处理，继续处理下一条数据
-	if (!isNationalVoice) continue;
-	const item = new Array(totalHeader.length).fill(complaintNumberDefault);
-	item[cityIndex] = city;
-	item[nameIndex] = name;
-	// 出账收入
-	item[outaccountIndex] = data ?? '';
-	// 是否AI
-	item[aiIndex] = isAI ? "是" : "否";
-	item[e55Index] = key;
+  // const [e55, complaintNumber] = key.split(totalSpilt);
+  // const item = data[key];
+  const { data = 0, isAI = false, city = '', name = '', isNationalVoice = false } = meridianData[key];
+  // 如果不是全国语音，不处理，继续处理下一条数据
+  if (!isNationalVoice) continue;
+  const item = new Array(totalHeader.length).fill(complaintNumberDefault);
+  item[cityIndex] = city;
+  item[nameIndex] = name;
+  // 出账收入
+  item[outaccountIndex] = data ?? '';
+  // 是否AI
+  item[aiIndex] = isAI ? '是' : '否';
+  item[e55Index] = key;
 
-	// 码号数量 账户并发 平均峰值并发
-	const { name: companyName = '', complaintNumber = '', name2 = "" } = mappingData[key] || {};
-	// 如果 companyName匹配不到，就使用name2匹配
-	const valueObj = trinaData[companyName] || trinaData[name2];
-	item[complaintNumberIndex] = complaintNumber;
-	if (valueObj) {
-		item[codeNumberIndex] = valueObj[trinakey2];
-		item[accountIndex] = valueObj[trinakey3];
-		item[averagePeakIndex] = valueObj[trinakey4];
-	}
-	// 投诉
-	if (complaintNumber !== complaintNumberDefault) {
-		const tempcomplaint = complainData[complaintNumber];
-		if (tempcomplaint) {
-			item[complaintsIndex] = tempcomplaint[complaintkey2]
-			item[collectionIndex] = tempcomplaint[complaintkey3]
-		}
-	}
-	result.push(item);
+  // 码号数量 账户并发 平均峰值并发
+  const { name: companyName = '', complaintNumber = '', name2 = '' } = mappingData[key] || {};
+  // 如果 companyName匹配不到，就使用name2匹配
+  const valueObj = trinaData[companyName] || trinaData[name2];
+  item[complaintNumberIndex] = complaintNumber;
+  if (valueObj) {
+    item[codeNumberIndex] = valueObj[trinakey2];
+    item[accountIndex] = valueObj[trinakey3];
+    item[averagePeakIndex] = valueObj[trinakey4];
+  }
+  // 投诉
+  if (complaintNumber !== complaintNumberDefault) {
+    const tempcomplaint = complainData[complaintNumber];
+    if (tempcomplaint) {
+      item[complaintsIndex] = tempcomplaint[complaintkey2];
+      item[collectionIndex] = tempcomplaint[complaintkey3];
+    }
+  }
+  result.push(item);
 }
 
-const buffer = xlsx.build([{ name: '汇总表', data: result }])
+const buffer = xlsx.build([{ name: '汇总表', data: result }]);
 fs.writeFileSync(`./output/汇总表.xlsx`, buffer);
