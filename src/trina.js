56fdc871a5bb6b8868e936dcf7b27331e49acff8
maxiao <m543438924@163.com>
2021-12-31 13:49:27 +0800

    feat:修改读取天合表后缀名

--- src/trina.js
+++ src/trina.js
@@ -1,6 +1,6 @@
 // 天合表
-const xlsx = require("node-xlsx");
-const fs = require("fs");
+const xlsx = require('node-xlsx');
+const fs = require('fs');
 const {
   inputDir,
   outputDir,
@@ -10,35 +10,34 @@ const {
   trinakey2,
   trinakey3,
   trinakey4,
-  replaceEnglishBracketsToChiniese,
-} = require('./../utils')
+  replaceEnglishBracketsToChiniese
+} = require('./../utils');
 const data = {};
 const indexMap = {};
 
-
-console.log('正在读取...', trinaName)
-const trina = xlsx.parse(`./${inputDir}/${trinaName}.xlsx`)[0];
+console.log('正在读取...', trinaName);
+const trina = xlsx.parse(`./${inputDir}/${trinaName}.xls`)[0];
 const trinaData = trina.data || [];
 
-console.log('正在处理...', trinaName)
+console.log('正在处理...', trinaName);
 trinaData.forEach((item, index) => {
   if (index === 0) {
     item.forEach((name, index) => {
       indexMap[name] = index;
-    })
+    });
   }
   if (index !== 0 && item[indexMap[trinakey1]]) {
     data[replaceEnglishBracketsToChiniese(item[indexMap[trinakey1]])] = {
       [trinakey2]: item[indexMap[trinakey2]],
       [trinakey3]: item[indexMap[trinakey3]],
-      [trinakey4]: item[indexMap[trinakey4]],
-    }
+      [trinakey4]: item[indexMap[trinakey4]]
+    };
   }
-})
+});
 // {
 //   '铸舜（上海）技术发展有限公司-后台添加': { '分配号码数': 1292, '平台配置并发数': 6000, '峰值': 45 },
 //   '卓越云讯（北京）科技有限公司': { '分配号码数': 7, '平台配置并发数': 150, '峰值': 7 }
 // }
 module.exports = {
   data: data
-}
+};
